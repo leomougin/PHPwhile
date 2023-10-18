39caeb6be53b702795d1511abfa6245aedb9cab9
Léo Mougin <mougin.leo@yahoo.com>
2023-10-18 10:48:12 +0200

    Revert "voici les tp while"
    This reverts commit e3703cd241429ec5053c26b625c7e2afcfe2ac89.

--- saisie-note.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$note=readline(" Saisir une note : ");
-while($note>20 || $note<0){
-    echo("La saisie de vote note est incorrect, elle doit être comprise entre 0 et 20 ! ");
-    echo PHP_EOL;
-    $note=readline(" Saisir une note : ");
-}
-echo(" Votre note est de correcte   ");
